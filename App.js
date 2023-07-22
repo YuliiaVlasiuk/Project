6380ef5eb9a3caf994eb1bddfd47b3fe367e76b3
YuliiaVlasiuk <vlasyukyulia@gmail.com>
2023-07-22 11:39:25 +0300

    all

--- App.js
+++ App.js
@@ -1,29 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, ImageBackground } from 'react-native';
-//import LoginScreen from './Screens/LoginScreen';
-import RegistrationScreen from './Screens/RegistrationScreen';
-//import { PostsScreen } from './Screens/PostsScreen';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, ImageBackground } from "react-native";
+import RegistrationScreen from "./Screens/RegistrationScreen";
 
 export default function App() {
-   return (
-     <View style={styles.container}>
-       <ImageBackground style={styles.image} source={require('./assets/images/PhotoBG.png')}>
-         <RegistrationScreen></RegistrationScreen>
-         </ImageBackground>
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        style={styles.image}
+        source={require("./assets/images/PhotoBG.png")}
+      >
+        <RegistrationScreen></RegistrationScreen>
+      </ImageBackground>
       <StatusBar style="auto" />
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
   },
   image: {
     flex: 1,
-    justifyContent: 'flex-end',
-    resizeMode: 'cover',
+    justifyContent: "flex-end",
+    resizeMode: "cover",
   },
 });
